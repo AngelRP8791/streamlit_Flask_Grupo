7520030a53cb0e743a940a8772b1e755ad900d66
AngelRP8791 <98138811+AngelRP8791@users.noreply.github.com>
2025-08-06 07:58:25 +0000

    Actualizar Proyecto

--- src/app.py
+++ src/app.py
@@ -71,5 +71,3 @@ if st.button("Realice Predicción"):
 if st.button("Ver Precisión del Modelo"):
     st.write("El Porcentaje de Precisión de su Modelo es:",f"{accuracy:.2%}")
     # resultados para otro 
-a=1
-b=4
